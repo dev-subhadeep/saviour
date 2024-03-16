cfbfca0cc255ba193e8c6702506351dd0f0a8ee8
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-17 00:46:36 +0530

    feat: Add responsive hero section with translucent background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,8 @@ import React from "react"
 
 const Hero = () => {
   return (
-    <div className="bg-[url('../../public/images/hero-image.png')] bg-cover py-[100px] relative">
-      <div className="w-[95vh] pt-[100px] bg-[#242323] opacity-40 absolute"></div>
-      <h1 className="text-white absolute text-[45px] font-family:'ZCOOL KuaiLe'">
+    <div className="bg-[url('../../public/images/hero-image.png')] bg-cover py-[100px]">
+      <h1 className="text-white text-[32pt] sm:text-[45pt] text-center container mx-auto w-[90%] my-6 bg-[#242323] bg-opacity-40 rounded-2xl p-6 sm:bg-opacity-0 sm:leading-[4rem]">
         Where Blockchain Heroes Thrive, Rescuing Dreams, Elevating Fortunes.
       </h1>
     </div>
