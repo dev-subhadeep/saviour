22b4c79d51528adfccb89b6d40058f37850c4374
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-16 19:46:55 +0530

    feat: Add accordions to FAQ add faqs as data in a separate file map through that data to create the accordions for FAQ

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,19 +1,31 @@
 import React from "react"
 import Accordion from "./ui/Accordion"
+import { faqs } from "../data/faqs"
+
+interface FAQ {
+  id: number
+  question: string
+  answer: string
+}
 
 const FAQ = () => {
   return (
     <section className="bg-background text-white">
       <div className="container p-10 mx-auto">
-        <Accordion
-          title={
-            <div className="flex gap-8">
-              <span>1</span>
-              <span>Why choose "saviour"?</span>
-            </div>
-          }
-          description="Cause it's the best!"
-        />
+        <div className="flex flex-col gap-4">
+          {faqs.map((faq: FAQ) => (
+            <Accordion
+              key={faq.id}
+              title={
+                <div className="flex gap-8">
+                  <span>{faq.id}</span>
+                  <span>{faq.question}</span>
+                </div>
+              }
+              description={faq.answer}
+            />
+          ))}
+        </div>
       </div>
     </section>
   )
