0bb4680553c14cec5cf1026110b0ec08813bb851
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-16 23:03:36 +0530

    refactor: create CardT2 Previously TokenInfo is now refactored Now the card can be used both for TokenInfo and TokenDetails for Tokenomics section

--- src/components/TokenInfo.tsx
+++ src/components/TokenInfo.tsx
@@ -1,26 +1,9 @@
 import React from "react"
 import Button from "./ui/Button"
+import CardT2 from "./CardT2"
 
 const TokenInfo = () => {
-  return (
-    <div className="text-white flex flex-col items-center">
-      <div className="bg-primary flex justify-center w-[150px] rounded-lg py-2 -mb-6">
-        Token Info
-      </div>
-      <div className=" border border-primary p-6 rounded-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-        aspernatur, porro, alias inventore doloribus beatae a voluptas quae
-        deserunt ducimus, non aut fugiat asperiores! Expedita dolor eveniet quo
-        totam autem! Aut, similique sed. Iusto quo illo enim, libero nobis
-        assumenda atque magni repellendus dolor ad omnis nihil, provident veniam
-        illum laboriosam tempore accusamus? Eaque quasi, numquam ullam debitis
-        reiciendis commodi?
-      </div>
-      <div className="mt-6">
-        <Button>Connect Wallet</Button>
-      </div>
-    </div>
-  )
+  return <CardT2 title="Token Info">Hi there</CardT2>
 }
 
 export default TokenInfo
