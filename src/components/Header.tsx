a71ec4f48c6f123684e29f1ce38a992256232b82
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-18 01:29:22 +0530

    feat: Mobile menu has animation store menu details separately for reusability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,20 +2,21 @@
 import React, { useState } from "react"
 import Logo from "../../public/saviour-logo.png"
 import Image from "next/image"
-import Navlinks from "./Navbar"
+import Navbar from "./Navbar"
 import Bars from "./ui/Bars"
 import ConnectButton from "./ConnectButton"
-import ChevronDown from "./ui/ChevronDown"
 import Link from "next/link"
 import { zcool } from "@/lib/fonts"
+import MobileNavMenu from "./MobileNavMenu"
 
-const Header = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+const Header: React.FC = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false)
+
   return (
     <>
       <header
         style={zcool.style}
-        className={`fixed flex justify-between items-center p-6 w-full text-white z-20 ${
+        className={`fixed flex justify-between items-center p-6 w-full text-white z-50 ${
           isSidebarOpen ? ` bg-background` : ``
         }`}
       >
@@ -32,34 +33,18 @@ const Header = () => {
           </Link>
         </div>
         <div className="hidden sm:flex">
-          <Navlinks />
+          <Navbar />
         </div>
         <div>
           <ConnectButton />
         </div>
       </header>
       <div
-        className={`fixed bg-background w-full overflow-hidden top-[100px] ${
+        className={`fixed z-40 bg-background w-full overflow-hidden top-[100px] flex flex-col justify-center transition-all ${
           isSidebarOpen ? `h-full` : `h-0`
         }`}
       >
-        <ul className="p-10 text-white">
-          <li key="1">
-            <Link href="/">Home</Link>
-          </li>
-          <li key="2">
-            <Link href="/#ido">IDO</Link>
-          </li>
-          <li key="3">
-            <Link href="/#tokenomics">Tokenomics</Link>
-          </li>
-          <li key="4">
-            <Link href="/#roadmap">Roadmap</Link>
-          </li>
-          <li key="5" className="flex items-center gap-1">
-            Coming Soon <ChevronDown />
-          </li>
-        </ul>
+        <MobileNavMenu setIsSidebarOpen={setIsSidebarOpen} />
       </div>
     </>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,17 @@
 import React from "react"
 import ChevronDown from "./ui/ChevronDown"
 import Link from "next/link"
+import MenuItems from "@/data/menu"
 
 const Navbar = () => {
   return (
     <ul className="flex gap-4 bg-background text-white px-4 py-2 rounded-2xl">
-      <li key="1">
-        <Link href="/">Home</Link>
-      </li>
-      <li key="2">
-        <Link href="/#ido">IDO</Link>
-      </li>
-      <li key="3">
-        <Link href="/#tokenomics">Tokenomics</Link>
-      </li>
-      <li key="4">
-        <Link href="/#roadmap">Roadmap</Link>
-      </li>
-      <li key="5" className="flex items-center gap-1">
-        Coming Soon <ChevronDown />
-      </li>
+      {MenuItems.map((item) => (
+        <li key={item.id} className="flex gap-2 justify-center items-center">
+          <Link href={item.url}>{item.label}</Link>{" "}
+          {item.submenu ? <ChevronDown /> : null}
+        </li>
+      ))}
     </ul>
   )
 }
